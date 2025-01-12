f3e1c110137c3bc36f5922f1e46c3d88721d7cf2
Eques72 <adrian.zareba@op.pl>
2025-01-12 22:09:17 +0100

    task 8 part 2 done

--- task8.rs
+++ task8.rs
@@ -20,7 +20,7 @@ fn read_file(rows: &mut Vec<Vec<char>>) -> io::Result<()>
 
 fn main() -> io::Result<()>
 {
-    let part = 1;
+    let part = 2;
     let mut rows: Vec<Vec<char>> = Vec::new();
 
     read_file(&mut rows)?;
@@ -46,26 +46,56 @@ fn main() -> io::Result<()>
         }
     }
 
-    for (key, value) in &sources {
-        for (i, j) in value {
-            for (k, m) in value {
-                if i == k && j == m
-                {
-                    continue;
-                }
-                let delta_x = i - k;
-                let delta_y = j - m;
-                let new_i = i + delta_x;
-                let new_j = j + delta_y;
-                if new_i >= 0 && new_i < height as i32 && new_j >= 0 && new_j < width as i32
-                {
-                    signals.insert((new_i as i32, new_j as i32));
+    if part == 1{
+        for (key, value) in &sources {
+            for (i, j) in value {
+                for (k, m) in value {
+                    if i == k && j == m
+                    {
+                        continue;
+                    }
+                    let delta_x = i - k;
+                    let delta_y = j - m;
+                    let new_i = i + delta_x;
+                    let new_j = j + delta_y;
+                    if new_i >= 0 && new_i < height as i32 && new_j >= 0 && new_j < width as i32
+                    {
+                        signals.insert((new_i as i32, new_j as i32));
+                    }
                 }
             }
         }
     }
+    else
+    {
+        for (key, value) in &sources {
+            for (i, j) in value {
+                for (k, m) in value {
+                    if i == k && j == m
+                    {
+                        continue;
+                    }
+                    signals.insert((*i as i32, *j as i32));
+                    let delta_x = i - k;
+                    let delta_y = j - m;
+                    let mut new_i = i + delta_x;
+                    let mut new_j = j + delta_y;
+                    while new_i >= 0 && new_i < height as i32 && new_j >= 0 && new_j < width as i32
+                    {
+                        signals.insert((new_i as i32, new_j as i32));
+                        // rows[new_i as usize][new_j as usize] = '#';
+                        new_i = new_i + delta_x;
+                        new_j = new_j + delta_y;
+                    }
+                }
+            }
+        }    
+    }
+
 
     println!("Signals: {}", signals.len());
+
+    // println!("{:?}", rows);
 
     Ok(())
 }
